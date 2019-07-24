d7658b85c7a876ce5088cebf13e298fac83ce16c
hassan ajaz <hassan@turnlunchon.com>
2019-07-24 18:50:35 +0500

    fix linter

--- bootstrap/expressApp.js
+++ bootstrap/expressApp.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const shortId = require('shortid');
 
 const config = require('../config/config');
+
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
@@ -14,15 +15,15 @@ app.set('view engine', 'pug');
 
 app.set('trust proxy', 1); // trust first proxy
 app.use(session({
-  secret: 'keyboard cat',
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: {}
+    cookie: {},
 }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: true,
 }));
 
 // multipart data handling
@@ -30,10 +31,10 @@ const storage = multer.diskStorage({
     destination: config.filePath,
     filename: (req, file, cb) => {
         cb(null, shortId.generate());
-    }
+    },
 });
 
-app.use(multer({storage: storage}).any());
+app.use(multer({ storage }).any());
 app.use(logger('dev'));
 
 module.exports = app;
