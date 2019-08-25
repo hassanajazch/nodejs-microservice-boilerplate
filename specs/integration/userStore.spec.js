bfeffa59b87398c861ca08e85a24e7fbd1c6e34c
hassan ajaz <hassan@turnlunchon.com>
2019-08-25 14:36:40 +0500

    Remove sequelize rc and add integration tests folder

---
+++ specs/integration/userStore.spec.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+
+/**
+ * Use JEST for testing
+ * Follow AAA rule for testing
+ */
+describe('User Store', () => {
+
+    it('should add user ', async function () {
+        // Arrange
+        // Act
+        // Asset
+    });
+
+
+});
