006210ffb454a893fcf7731dbccbea8ed7f1c5ec
hassan ajaz <hassan@turnlunchon.com>
2019-08-25 15:08:19 +0500

    change directory name from bootstrap to server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const config = require("./config/config");
-const server = require("./bootstrap");
+const server = require("./server");
 
 server.listen(`${config.port}`, () => {
     console.log(`Server now listening at localhost:${config.port}`);
